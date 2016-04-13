a75b1d8dcea784a8df0cbf47ea6d1211ba6b8cf1
Tadukoo <realtadukoo@gmail.com>
2016-04-13 09:32:00 -0400

    Setup basics for being able to test the database.

---
+++ src/edu/ycp/cs320/derp/model/User.java
@@ -0,0 +1,47 @@
+package edu.ycp.cs320.derp.model;
+
+public class User{
+	private int userId;
+	private String name;
+	private String email;
+	private String ip;
+	
+	public User(int userId, String name, String email, String ip){
+		this.userId = userId;
+		this.name = name;
+		this.email = email;
+		this.ip = ip;
+	}
+	
+	public int getUserId(){
+		return userId;
+	}
+	
+	public void setUserId(int userId){
+		this.userId = userId;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getEmail(){
+		return email;
+	}
+	
+	public void setEmail(String email){
+		this.email = email;
+	}
+	
+	public String getIP(){
+		return ip;
+	}
+	
+	public void setIP(String ip){
+		this.ip = ip;
+	}
+}
